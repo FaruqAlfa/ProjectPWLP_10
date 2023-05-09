e55b1fc2d59b2ef12825e1bb0f8eb0bede0b9c1e
FaruqAlfa <alfafahmi172@gmail.com>
2023-05-09 13:06:01 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ Route::resource('articles', ArticleController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
